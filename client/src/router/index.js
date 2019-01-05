7296907674070577b562545307fd91e068ad5862
ChrisOB5150 <chrisob5150@gmail.com>
2019-01-05 16:38:46 +0200

    end of first video with working register functionality

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Blank from '@/components/Blank'
+import Register from '@/components/Register'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Blank',
       component: Blank
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
